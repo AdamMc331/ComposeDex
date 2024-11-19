a3f3b9dd1c6a96614c77ee626e621d0957b19724
Adam McNeilly <amcneilly331@gmail.com>
2024-11-18 21:52:21 -0500

    Adding test to call data.

--- app/src/main/java/com/adammcneilly/pokedex/di/RemoteModule.kt
+++ app/src/main/java/com/adammcneilly/pokedex/di/RemoteModule.kt
@@ -1,9 +1,14 @@
 package com.adammcneilly.pokedex.di
 
 import com.apollographql.apollo.ApolloClient
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 object RemoteModule {
     @Provides
     @Singleton

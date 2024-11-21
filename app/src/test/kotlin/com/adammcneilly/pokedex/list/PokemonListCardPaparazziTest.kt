c50bcbbf11590d8c50abba6f582eabed6fec1aae
Adam McNeilly <amcneilly331@gmail.com>
2024-11-20 20:39:15 -0500

    Import fix?

--- app/src/test/kotlin/com/adammcneilly/pokedex/list/PokemonListCardPaparazziTest.kt
+++ app/src/test/kotlin/com/adammcneilly/pokedex/list/PokemonListCardPaparazziTest.kt
@@ -3,7 +3,6 @@ package com.adammcneilly.pokedex.list
 import com.adammcneilly.pokedex.BasePaparazziTest
 import com.adammcneilly.pokedex.displaymodels.ImageDisplayModel
 import com.adammcneilly.pokedex.displaymodels.PokemonDisplayModel
-import com.adammcneilly.pokedex.list.PokemonListCard
 import com.adammcneilly.pokedex.models.PokemonType
 import org.junit.Test
 
